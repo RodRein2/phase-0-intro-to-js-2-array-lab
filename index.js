7bc3a29137cac1cb979495f10bbde5570c19cce5
Ana Rodriguez Reiner <arodriguezreiner@outlook.com>
2023-05-17 13:55:16 -0700

    commit lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+];
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name) {
+    const copyOfCats= cats.slice()
+    copyOfCats.push(name)
+    return copyOfCats
+}
+function prependCat(name) {
+    const copyOfCats = cats.slice()
+    copyOfCats.unshift(name)
+    return copyOfCats
+}
+function removeLastCat() {
+    const copyOfCats = cats.slice()
+    copyOfCats.pop()
+    return copyOfCats
+}
+function removeFirstCat() {
+    const copyOfCats = cats.slice()
+    copyOfCats.shift()
+    return copyOfCats
+}
+
